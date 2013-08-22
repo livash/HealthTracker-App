25f646e32bb5ebd727197838ee3d1520ebf63a5e
olena ivashyna <olena.ivashyna@gmail.com>
2013-08-22 14:04:55 -0700

    Devise gem, User model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ HealthTracker::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #addition for Devise gem
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
