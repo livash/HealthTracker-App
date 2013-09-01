0ea62064e801defb96b7ec4218f16fbbf91af8a9
olena ivashyna <olena.ivashyna@gmail.com>
2013-09-01 14:40:36 -0700

    add username to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,16 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
+  validates :username, :email, presence: true
+  
+  before_validation :ensure_username_has_a_value
+  
   has_many :meals, :foreign_key => :user_id
+  
+  protected
+  def ensure_username_has_a_value
+    if username.nil?
+      self.username = email.split("@").first unless email.blank?
+    end 
+  end
 end
